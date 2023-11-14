fea02053d9550b75c4a689e5f1f9fc191cc291d4
Attirobert <karobatti@gmail.com>
2023-11-14 18:56:07 +0100

    IktatoMySql elkezdve

---
+++ IktatoMySql/Database.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace IktatoMySql
+{
+    class Database
+    {
+        static readonly string server = "127.0.0.1",
+            user = "root",
+            password = "",
+            database = "iktato";
+
+        public static string connection_string = $"SERVER='{server}'; USER='{user}'; DATABASE='{database}'; PASSWORD='{password}'";
+
+        public MySqlConnection mySqlConnection = new MySqlConnection(connection_string);
+
+        public Database()
+        {
+        }
+
+        public bool connect_db()
+        {
+            try
+            {
+                mySqlConnection.Open();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        public bool open_db()
+        {
+            try
+            {
+                mySqlConnection.Open();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        public bool close_db()
+        {
+            try
+            {
+                mySqlConnection.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
