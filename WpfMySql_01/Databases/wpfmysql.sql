7611fa23befeb6761ac6e169c10b15eb75dbd219
Attirobert <karobatti@gmail.com>
2024-01-02 18:46:00 +0100

    Adatbázis aktualizálás

--- WpfMySql_01/Databases/wpfmysql.sql
+++ WpfMySql_01/Databases/wpfmysql.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2023. Dec 18. 18:58
+-- Létrehozás ideje: 2024. Jan 02. 18:43
 -- Kiszolgáló verziója: 10.4.28-MariaDB
 -- PHP verzió: 8.1.17
 
@@ -20,6 +20,22 @@ SET time_zone = "+00:00";
 --
 -- Adatbázis: `wpfmysql`
 --
+CREATE DATABASE IF NOT EXISTS `wpfmysql` DEFAULT CHARACTER SET utf8 COLLATE utf8_hungarian_ci;
+USE `wpfmysql`;
+
+DELIMITER $$
+--
+-- Eljárások
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `DeletePerson` (IN `pId` INT(11))   DELETE FROM `persons` WHERE `id` = pId$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `InsertPerson` (IN `pName` VARCHAR(20))   INSERT INTO `persons`(`name`) VALUES (pName)$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `PersonsAll` ()   Select * from persons$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `UpdatePerson` (IN `pName` VARCHAR(20), IN `pId` INT(11))   UPDATE `persons` SET `name`= pName WHERE `id`= pId$$
+
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -29,16 +45,22 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `persons` (
   `id` int(11) NOT NULL,
-  `name` varchar(20) NOT NULL
+  `name` varchar(20) NOT NULL,
+  `Jelszo` varchar(10) NOT NULL,
+  `Admin` tinyint(1) NOT NULL DEFAULT 0,
+  `Neme` varchar(5) NOT NULL DEFAULT 'Férfi',
+  `Szulev` date DEFAULT NULL,
+  `Email` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
 
 --
 -- A tábla adatainak kiíratása `persons`
 --
 
-INSERT INTO `persons` (`id`, `name`) VALUES
-(1, 'Attila'),
-(2, 'Gábor');
+INSERT INTO `persons` (`id`, `name`, `Jelszo`, `Admin`, `Neme`, `Szulev`, `Email`) VALUES
+(1, 'Attila', '', 0, 'Férfi', NULL, ''),
+(7, 'Zsuzsika', '', 0, 'Férfi', NULL, ''),
+(9, 'István', '', 0, 'Férfi', NULL, '');
 
 --
 -- Indexek a kiírt táblákhoz
@@ -59,7 +81,7 @@ ALTER TABLE `persons`
 -- AUTO_INCREMENT a táblához `persons`
 --
 ALTER TABLE `persons`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
